1130fae8c0c9904066541171497f3c0abc8da08c
Nursaputra <nursaputra.developer@gmail.com>
2024-12-03 22:09:08 +0700

    Add Onboarding ViewModel

---
+++ app/src/main/java/com/nursaputrayow/artefacto/viewmodel/OnboardingViewModel.kt
@@ -0,0 +1,16 @@
+package com.nursaputrayow.artefacto.viewmodel
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class OnboardingViewModel : ViewModel() {
+    private val _currentStep = MutableStateFlow(0)
+    val currentStep: StateFlow<Int> = _currentStep
+
+    fun nextStep() {
+        if (_currentStep.value < 2) {
+            _currentStep.value++
+        }
+    }
+}
