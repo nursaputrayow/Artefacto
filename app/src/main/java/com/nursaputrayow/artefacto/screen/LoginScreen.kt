61a50dca6424be43f5b31a6ba9c5a2f86a17192d
Nursaputra <nursaputra.developer@gmail.com>
2024-12-03 22:09:27 +0700

    Add Login Screen

--- app/src/main/java/com/nursaputrayow/artefacto/screen/LoginScreen.kt
+++ app/src/main/java/com/nursaputrayow/artefacto/screen/LoginScreen.kt
@@ -1,4 +1,18 @@
 package com.nursaputrayow.artefacto.screen
 
-class LoginScreen {
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoginScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Login Screen")
+    }
 }
