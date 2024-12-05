9ae5716aec3e802859d0da5a183632c7a47833da
Nursaputra <nursaputra.developer@gmail.com>
2024-12-05 21:25:29 +0700

    Update SplashScreen

--- app/src/main/java/com/nursaputrayow/artefacto/screen/SplashScreen.kt
+++ app/src/main/java/com/nursaputrayow/artefacto/screen/SplashScreen.kt
@@ -1,6 +1,12 @@
 package com.nursaputrayow.artefacto.screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -8,6 +14,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.nursaputrayow.artefacto.viewmodel.SplashScreenViewModel
 import com.nursaputrayow.artefacto.R
 
@@ -30,11 +37,18 @@ fun SplashScreenContent(
 @Composable
 fun SplashScreen() {
     Surface(
-        color = MaterialTheme.colorScheme.background
+        color = MaterialTheme.colorScheme.background,
+        modifier = Modifier.fillMaxSize()
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.logo),
-            contentDescription = "App Logo"
-        )
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Splash Screen Logo",
+                modifier = Modifier.size(200.dp).padding(16.dp)
+            )
+        }
     }
 }
