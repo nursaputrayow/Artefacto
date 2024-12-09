5cd9e762c8d6c254767155f2e4f1131a4226d255
Nursaputra <nursaputra.developer@gmail.com>
2024-12-10 01:53:36 +0700

    compileSdk 34 to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.nursaputrayow.artefacto"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.nursaputrayow.artefacto"
