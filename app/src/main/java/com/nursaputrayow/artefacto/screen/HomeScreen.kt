6d36100b551e70df856d62ede21ce3cfbc4cd105
Nursaputra <nursaputra.developer@gmail.com>
2024-12-11 23:47:07 +0700

    Update home

---
+++ app/src/main/java/com/nursaputrayow/artefacto/screen/HomeScreen.kt
@@ -0,0 +1,26 @@
+package com.nursaputrayow.artefacto.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFFFFFFF)),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Home Screen",
+            style = MaterialTheme.typography.headlineMedium
+        )
+    }
+}
